1d1adf707c98f6ce2886f74d8caad50ff5bb5a40
asker <askersultanov666@gmail.com>
2025-02-26 23:09:14 +0300

    write schema for itemStatus

--- src/schemas/item.schema.ts
+++ src/schemas/item.schema.ts
@@ -3,6 +3,23 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type ItemStatusDocument = HydratedDocument<ItemStatus>;
 
-class ItemStatus {}
+@Schema({ _id: false })
+class Order {
+  @Prop()
+  id: string;
+  @Prop({ type: [String] })
+  items: string[];
+  @Prop({ type: [String] })
+  itemId: string[];
+}
+
+@Schema()
+class ItemStatus {
+  @Prop()
+  userId: string;
+
+  @Prop({ type: [Order] })
+  orders: Order[];
+}
 
 export var ItemStatusSchema = SchemaFactory.createForClass(ItemStatus);
