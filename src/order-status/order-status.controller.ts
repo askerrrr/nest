932bc0870c5be47bbe040d78865679a12d2c53a7
asker <askersultanov666@gmail.com>
2025-02-26 20:18:45 +0300

    make path orderstatus

--- src/order-status/order-status.controller.ts
+++ src/order-status/order-status.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Get, Patch, Param, Controller } from '@nestjs/common';
 import { OrderStatusService } from './order-status.service';
 
 @Controller('order-status')
 export class OrderStatusController {
   constructor(private readonly orderStatusService: OrderStatusService) {}
+  @Get('api/:userId/:orderId')
+  async getOrderStatus(@Param() param) {
+    return await this.orderStatusService.getOrderStatus(
+      param.userId,
+      param.orderId,
+    );
+  }
+
+  @Patch('/:userId/:orderId/:status')
+  async changeOrderStatus(@Param() param) {
+    return await this.orderStatusService.changeOrderStatus(
+      param.userId,
+      param.orderId,
+      param.status,
+    );
+  }
 }
--- src/order-status/order-status.service.ts
+++ src/order-status/order-status.service.ts
@@ -1,4 +1,57 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
-export class OrderStatusService {}
+export class OrderStatusService {
+  constructor(@InjectModel(User.name) private user: Model<UserDocument>) {}
+
+  async getOrderStatus(userId, orderId) {
+    var document = await this.user.findOne({
+      userId,
+      'orders.order.id': orderId,
+    });
+
+    var orderData = document?.orders.find((e) => e.order.id == orderId);
+    var orderStatus = orderData?.order.orderStatus;
+    return orderStatus;
+  }
+
+  async changeOrderStatus(userId, orderId, status) {
+    var [statusValue, statusId] = status.split(':');
+
+    statusId = statusId.split('').reverse()[0];
+
+    var orderStatus = statusValue + ':' + statusId;
+    await this.sendOrderStatusUpdate(userId, orderId, orderStatus);
+    await this.user.updateOne(
+      { userId, 'orders.order.id': orderId },
+      {
+        $set: { 'orders.$.order.orderStatus': status },
+      },
+    );
+  }
+
+  async sendOrderStatusUpdate(userId, orderId, orderStatus) {
+    var response = await fetch('env.bot_server_ip', {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${'env.bearer_token'}`,
+      },
+      body: JSON.stringify({
+        userId,
+        orderId,
+        orderStatus,
+      }),
+    });
+
+    if (!response.ok) {
+      var err = await response.text();
+      console.log('Ошибка при отправлении статуса боту: ', err);
+      return;
+    }
+  }
+}
