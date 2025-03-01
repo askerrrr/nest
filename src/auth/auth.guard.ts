66cb9d90594c9da8546fe926ea3d26abd98f572c
asker <askersultanov666@gmail.com>
2025-03-01 14:24:36 +0300

    add redirect in AuthGuard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,20 +5,23 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = request.cookies?.token;
+    var request = context.switchToHttp().getRequest();
+    var response = context.switchToHttp().getResponse<Response>();
+    var token = request.cookies?.token;
 
     if (!token) {
+      response.redirect('/auth/login');
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
+      var payload = await this.jwtService.verifyAsync(token, {
         secret: 'asker',
       });
 
