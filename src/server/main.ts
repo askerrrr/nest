88ab15a01a131f0dd57b2ebd233be9baabf958e1
asker <askersultanov666@gmail.com>
2025-03-18 17:57:35 +0300

    add helmet

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,17 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { RootModule } from './root/root.module';
 
 (async () => {
   const app = await NestFactory.create(RootModule);
+  app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: true,
+      directives: { 'img-src': ["'self'", 'https: data:'] },
+    }),
+  );
+
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 })();
