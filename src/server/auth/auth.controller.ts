28e0ef41f0c62272efd52ffc7b25bf722df21591
asker <askersultanov666@gmail.com>
2025-03-18 17:38:58 +0300

    change project structure

---
+++ src/server/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { join } from 'path';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { Get, Res, Post, Body, Controller } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('login')
+  async getAuthForm(@Res() res: Response) {
+    console.log(__dirname);
+    return res.sendFile(
+      join(__dirname, '/../../src/client/html/authForm.html'),
+    );
+  }
+  @Post('login/check')
+  async checkLogin(@Body() body: Object, @Res({ passthrough: true }) res) {
+    var token = await this.authService.checkLogin(body);
+
+    if (!token) {
+      res.redirect('/auth/login');
+    }
+
+    res.cookie('token', token, {
+      maxAge: 1000 * 60 * 60,
+      httpOnly: true,
+    });
+
+    return { redirect: true };
+  }
+}
---
+++ src/server/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'asker',
+      signOptions: { expiresIn: '1h' },
+    }),
+    ServeStaticModule.forRoot({
+      serveRoot: '/auth',
+      rootPath: join(__dirname, '../../src/client'),
+    }),
+  ],
+})
+export class AuthModule {}
